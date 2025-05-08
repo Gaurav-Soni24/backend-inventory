10698a85b465f2b79afcf6b7ac51599eaf3cd19c
Gaurav Soni <115176423+Gaurav-Soni24@users.noreply.github.com>
2025-05-08 17:33:52 +0530

    Update index.js

--- index.js
+++ index.js
@@ -31,6 +31,12 @@ const app = express();
 const port = 8080;
 
 app.use(express.json());
+app.use((req, res, next) => {
+  res.header('Access-Control-Allow-Origin', '*');
+  res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
+  res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
+  next();
+});
 
 // Test endpoint
 app.get('/', (req, res) => {
